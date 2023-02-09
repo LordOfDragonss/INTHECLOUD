4de9c6f0e7c2f793b20133b1a8814759c4fef7aa
mike.tool <mike.tool@hva.nl>
2023-02-09 16:54:52 +0100

    Added project files, tutorial flask

---
+++ Cloud Solutions/hello_flask/app.py
@@ -0,0 +1,56 @@
+import re
+from datetime import datetime
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html",)
+
+@app.route("/blog")
+def blog():
+    
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Jack'},
+            'body': 'Peter is mean to me :('
+        }
+    ]
+    return render_template("blog.html", posts = posts)
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
